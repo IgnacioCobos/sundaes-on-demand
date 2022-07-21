2c301810f6ee5eae0660ab8e7f3a166e1a8c28fe
IgnacioCobos <ignacio.cobos.gomez@gmail.com>
2022-07-21 19:30:44 +0200

    Test and code context for scoops subtotal

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import logo from "./logo.svg";
-import "./App.css";
+import Container from 'react-bootstrap/Container';
+import OrderEntry from './pages/entry/OrderEntry';
+import { OrderDetailsProvider } from './contexts/OrderDetails';
 import SummaryForm from "./pages/summary/SummaryForm";
 
 function App() {
   return (
-    <SummaryForm/>
+    <Container>
+      <OrderDetailsProvider>
+        {/* Summary page and entry page need provider */}
+        <OrderEntry/>
+      </OrderDetailsProvider>
+      {/* confirmation page does not need provider */}
+    </Container>
   );
 }
 
